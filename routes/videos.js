98884ec55e93d5b1a039fe4eb2531e772d92a37d
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-06-08 00:46:00 -0700

    add get /videos request in video.js to read full json data

--- routes/videos.js
+++ routes/videos.js
@@ -1,15 +1,44 @@
+const fs = require("fs");
 const express = require("express");
 const router = express.Router();
 
+function readVideoList(file, callback) {
+    fs.readFile(file, "utf-8", (err, data) => {
+        console.log(data);
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("data read");
+        }
+        callback(data);
+    })
+}
+
 router.route("/")
     .get((req, res) => {
-        // res.send()
+        readVideoList("./data/videos.json", (data) => {
+            //send only partial of array of obj data
+            // for (let i of data) {
+            //     let list = 
+            //     res.send(list);
+            // }
+            res.send(data);
+        })
     })
-    .post((req, res) => {
-        // res.send()
-    })
+//     .post((req, res) => {
+//         // res.send()
+//     })
 
-router.get("/:id", (req, res) => {
-    // send the data of that item in the json arr
-})
+// router.get("/:id", (req, res) => {
+//     // send the data of that item in the json arr
+// })
 
+
+module.exports = router;
+
+// fs.readFile("./data/videos.json", "utf-8", (err, fileData) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(fileData);
+// })
